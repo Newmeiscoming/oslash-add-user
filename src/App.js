e1d864fc5b8399faf2651e526bbeaefb7a35d461
Abhishek Shivhare <abhishekshivhare56@gmail.com>
2022-11-11 12:51:48 +0530

    button created

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import {HiShare} from "react-icons/hi"
+import Input from './Components/Input';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <h2>Click on Share Button</h2>
+      <button>Share &nbsp;<HiShare/></button> */}
+      <Input/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Input.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../styles/Input.css"
+import {MdPublic} from"react-icons/md"
+
+const Input = () => {
+  return (
+    <div>
+        <section><MdPublic/></section>
+    </div>
+  )
+}
+
+export default Input
